5463c3abf3899ab6be1169da39c906965462bab5
Irene Penuelas <IBarraza445@west-mec.org>
2018-03-16 13:12:23 -0700

    photos and content edited
    sidebar fixed on mobile

--- layout/scripts/jquery.mobilemenu.js
+++ layout/scripts/jquery.mobilemenu.js
@@ -11,4 +11,50 @@ Thanks to:
 "Submenu's with a dash" Daryn St. Pierre - http://jsfiddle.net/bloqhead/Kq43X/
 */
 
-$('<form action="#"><select /></form>').appendTo("#mainav");$("<option />",{selected:"selected",value:"",text:"MENU"}).appendTo("#mainav select");$("#mainav a").each(function(){var e=$(this);if($(e).parents("ul ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"- - - "+e.text()}).appendTo("#mainav select")}else if($(e).parents("ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"- - "+e.text()}).appendTo("#mainav select")}else if($(e).parents("ul").length>=1){$("<option />",{value:e.attr("href"),text:""+e.text()}).appendTo("#mainav select")}else{$("<option />",{value:e.attr("href"),text:e.text()}).appendTo("#mainav select")}});$("#mainav select").change(function(){if($(this).find("option:selected").val()!=="#"){window.location=$(this).find("option:selected").val()}})
+/* MAIN NAV - DO NOT EDIT */
+$('<form action="#"><select /></form>').appendTo("#mainav");
+$("<option />",{selected:"selected",value:"",text:"MENU"}).appendTo("#mainav select");
+$("#mainav a").each(function(){
+    var e = $(this);
+    if($(e).parents("ul ul ul").length >= 1){
+        $("<option />",{value:e.attr("href"),text:"- - - "+e.text()}).appendTo("#mainav select")
+    }
+    else if($(e).parents("ul ul").length>=1){
+            $("<option />",{value:e.attr("href"),text:"- - "+e.text()}).appendTo("#mainav select")
+        }
+    else if($(e).parents("ul").length>=1){
+            $("<option />",{value:e.attr("href"),text:""+e.text()}).appendTo("#mainav select")
+        }
+    else{
+        $("<option />",{value:e.attr("href"),text:e.text()}).appendTo("#mainav select")
+        }
+});
+$("#mainav select").change(function(){
+    if($(this).find("option:selected").val() !== "#"){
+    window.location=$(this).find("option:selected").val()
+    }
+});
+
+/* SIDEBAR */
+// Create the dropdown base
+$("<select />").appendTo(".sidebar");
+
+// Create default option "Go to..."
+$("<option />", {
+   "selected": "selected",
+   "value"   : "",
+   "text"    : "Go to..."
+}).appendTo(".sidebar select");
+
+// // Populate dropdown with menu items
+$(".sidebar a").each(function() {
+ var el = $(this);
+ $("<option />", {
+     "value"   : el.attr("href"),
+     "text"    : el.text()
+ }).appendTo(".sidebar select");
+});
+
+$(".sidebar select").change(function() {
+    window.location = $(this).find("option:selected").val();
+  });
